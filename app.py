977683b5094677f6f8bfb3f563df26daa12c0214
Myagrofu1205 <ddan72813@gmail.com>
2025-06-30 19:00:59 +0900

    add quiz 1

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
+quiz_answer=os.getenv("TEST")
+print(quiz_answer)
+
 @app.route('/')
 def index():
-    return render_template("index.html")
+    if quiz_answer=="correct":
+        return render_template("index.html")
+    else:
+        return render_template("no.html")
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0', port=8000)
